98c033460a5e404f59bf2c3e0825d26fc09fef02
Scott W Harden <swharden@gmail.com>
2024-04-03 08:40:45 -0400

    Plot: example object with axis limits

---
+++ src/MyBackend/Plot.cs
@@ -0,0 +1,21 @@
+﻿namespace MyBackend;
+
+/// <summary>
+/// An example object representing a plot with axis limits
+/// that can be manipulated by the user.
+/// </summary>
+public class Plot
+{
+    public double Left { get; private set; } = -10;
+    public double Right { get; private set; } = 10;
+    public double Bottom { get; private set; } = -10;
+    public double Top { get; private set; } = 10;
+
+    public void Pan(double x, double y)
+    {
+        Left += x;
+        Right += x;
+        Bottom += y;
+        Top += y;
+    }
+}
