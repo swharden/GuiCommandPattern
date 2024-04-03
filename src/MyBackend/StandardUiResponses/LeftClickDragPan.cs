30d34dacd05cfcd152baac981074e9c60d3b7da9
Scott W Harden <swharden@gmail.com>
2024-04-03 08:41:33 -0400

    create a UI event manager and example response

---
+++ src/MyBackend/StandardUiResponses/LeftClickDragPan.cs
@@ -0,0 +1,30 @@
+﻿
+namespace MyBackend.StandardUiResponses;
+
+public class LeftClickDragPan : IUiResponse
+{
+    public bool WillExecute(List<UiEvent> uiEvents, Plot plot)
+    {
+        bool hasMouseDown = uiEvents.First().Name == "left button down";
+        bool hasMouseUp = uiEvents.Last().Name == "left button up";
+        bool isDragAndDrop = hasMouseDown && hasMouseUp;
+        if (!isDragAndDrop)
+            return false;
+
+        double dragX = uiEvents.Last().X - uiEvents.First().X;
+        double dragY = uiEvents.Last().X - uiEvents.First().X;
+        double drag = Math.Sqrt(dragX * dragX + dragY * dragY);
+        if (drag == 0)
+            return false;
+
+        return true;
+    }
+
+    public void Execute(List<UiEvent> uiEvents, Plot plot)
+    {
+        double dragX = uiEvents.Last().X - uiEvents.First().X;
+        double dragY = uiEvents.Last().X - uiEvents.First().X;
+        plot.Pan(dragX, dragY);
+        uiEvents.Clear();
+    }
+}
---
+++ src/MyBackend/UiEventManager.cs
@@ -0,0 +1,51 @@
+﻿namespace MyBackend;
+
+public class UiEventManager(Plot plot)
+{
+    /// <summary>
+    /// A growing list of UI inputs.
+    /// The list is cleared when action is taken.
+    /// </summary>
+    public List<UiEvent> Events { get; } = [];
+
+    /// <summary>
+    /// A list of rules for how to respond to UI events,
+    /// in the order they will be applied.
+    /// </summary>
+    public List<IUiResponse> Responses { get; } =
+    [
+        new StandardUiResponses.LeftClickDragPan(),
+    ];
+
+    /// <summary>
+    /// The plot which will be modified by responses
+    /// </summary>
+    private Plot Plot { get; } = plot;
+
+    public void Add(UiEvent uiEvent, bool process = true)
+    {
+        Events.Add(uiEvent);
+
+        if (process)
+            ProcessEvents();
+    }
+
+    public void Add(string name, double x, double y, bool process = true)
+    {
+        UiEvent uiEvent = new(name, x, y);
+        Add(uiEvent, process);
+    }
+
+    /// <summary>
+    /// Evaluate possible responses in order and apply
+    /// them to the plot if they are a match.
+    /// </summary>
+    private void ProcessEvents()
+    {
+        foreach (IUiResponse response in Responses)
+        {
+            if (response.WillExecute(Events, Plot))
+                response.Execute(Events, Plot);
+        }
+    }
+}
