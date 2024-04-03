38474853b251fcc782c6e6ac66daff5f5229bb7b
Scott W Harden <swharden@gmail.com>
2024-04-03 08:41:17 -0400

    create interfaces for UI events and responses

---
+++ src/MyBackend/IUiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace MyBackend;
+
+/// <summary>
+/// Contains logic that responds to UI events by acting on a Plot
+/// </summary>
+public interface IUiResponse
+{
+    public bool WillExecute(List<UiEvent> uiEvents, Plot plot);
+    public void Execute(List<UiEvent> uiEvents, Plot plot);
+}
---
+++ src/MyBackend/UiEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace MyBackend;
+
+public readonly struct UiEvent(string name, double x, double y)
+{
+    public string Name { get; } = name;
+    public double X { get; } = x;
+    public double Y { get; } = y;
+}
