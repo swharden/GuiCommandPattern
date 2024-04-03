72e725bc8af6e64fa927fcf96a0b5f6552c71632
Scott W Harden <swharden@gmail.com>
2024-04-03 08:41:43 -0400

    test: left click drag response

--- src/MyBackendTests/UnitTest1.cs
+++ src/MyBackendTests/UnitTest1.cs
@@ -1,11 +1,19 @@
+using MyBackend;
+
 namespace MyBackendTests;
 
 public class Tests
 {
     [Test]
-    public void Test_Debug_Math()
+    public void Test_Pan()
     {
-        int result = MyBackend.BackendDebug.Add(2, 3);
-        Assert.That(result, Is.EqualTo(5));
+        Plot plot = new();
+        UiEventManager eventMan = new(plot);
+        Assert.That(plot.Right, Is.EqualTo(10));
+
+        eventMan.Add("left button down", 111, 222);
+        eventMan.Add("mouse move", 123, 234);
+        eventMan.Add("left button up", 222, 333);
+        Assert.That(plot.Right, Is.GreaterThan(10));
     }
 }
